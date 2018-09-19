d97155583ad6b94e68b01a2c33d55f8e21cbdadd
sheetal <sheetal.bv@dell.com>
2018-09-19 15:40:46 +0530

    Updating quick sort code.

--- algo/quick_sort_1.py
+++ algo/quick_sort_1.py
@@ -1,24 +1,20 @@
-from sys import getrecursionlimit, setrecursionlimit
-
-def partition(input_array, start, end):
-    pivot = input_array[end]
-    p_index = ( start - 1 )
-    for index in range(start, end):
-        if input_array[index] <= pivot:
+def partition(array, start, end):
+    p_index = (start-1 )
+    pivot = array[end]
+    for i in range(start, end):
+        if array[i] <= pivot:
             p_index += 1
-            input_array[index], input_array[p_index] = input_array[p_index], input_array[index]
-    input_array[p_index+1], input_array[end] = input_array[end], input_array[p_index+1]
+            array[p_index], array[i] = array[i], array[p_index]
+    array[p_index+1], array[end] = array[end], array[p_index+1]
     return p_index+1
 
-def quick_sort(input_array, start, end):
+def quick_sort(array, start, end):
     if start < end:
-        p_index = partition(input_array, start, end)
-        quick_sort(input_array, start, p_index-1)
-        quick_sort(input_array, p_index+1, end)
-    return    
+        p_indx = partition(array, start, end)
 
-test_list = [8, 2,6,4,1,3,5, 9]
-print(test_list)
-setrecursionlimit(3500)
-quick_sort(test_list, 0, len(test_list)-1)
-print(test_list)
+        quick_sort(array, start, p_indx-1)
+        quick_sort(array, p_indx+1, end)
+
+t = [3,1,5,7,9,8,6,4,2]
+quick_sort(t, 0, len(t)-1)
+print(t)
