970f0af7ef9196d01182b989b928167c9e973e93
Jon-J <bv.sheetalkumar@gmail.com>
2018-10-17 22:27:14 +0530

    up

---
+++ algo/dynamic_programming/dp_fib.py
@@ -0,0 +1,16 @@
+def fib(n, lookup):
+    if n == 0 or n == 1:
+        lookup[n] = n
+
+    if lookup[n] is None:
+        lookup[n] = fib(n-1, lookup) + fib(n-2, lookup)
+
+    return lookup[n]
+
+def main():
+    n = 50 
+    lookup = [None] * (n+1)
+    print(fib(n,lookup))
+
+if __name__ == "__main__":
+    main()
