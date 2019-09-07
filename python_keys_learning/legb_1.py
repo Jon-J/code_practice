8b906f445731ca2acea22892a9b189a090b49eda
Jon-J <bv.sheetalkumar@gmail.com>
2019-09-07 18:52:03 +0530

    update

---
+++ python_keys_learning/legb_1.py
@@ -0,0 +1,59 @@
+a_var = 'global variable'
+
+def a_func():
+    print(a_var, '[ a_var inside a_func() ]')
+
+a_func()
+print(a_var, '[ a_var outside a_func() ]')
+
+print('***************************************')
+a_var = 'global value'
+
+def a_func():
+    a_var = 'local value'
+    print(a_var, '[ a_var inside a_func() ]')
+
+a_func()
+print(a_var, '[ a_var outside a_func() ]')
+print('***************************************')
+
+a_var = 'global value'
+
+def a_func():
+    global a_var
+    a_var = 'local value'
+    print(a_var, '[ a_var inside a_func() ]')
+
+print(a_var, '[ a_var outside a_func() ]')
+a_func()
+print(a_var, '[ a_var outside a_func() ]')
+print('***************************************')
+
+a_var = 'global value'
+
+def outer():
+    a_var = 'enclosed value'
+
+    def inner():
+        a_var = 'local value'
+        print(a_var)
+
+    inner()
+
+outer()
+print('***************************************')
+
+a_var = 'global variable'
+
+def len(in_var):
+    print('called my len() function')
+    l = 0
+    for i in in_var:
+        l += 1
+    return l
+
+def a_func(in_var):
+    len_in_var = len(in_var)
+    print('Input variable is of length', len_in_var)
+
+a_func('Hello, World!')
--- python_keys_learning/zip_unzip.py
+++ python_keys_learning/zip_unzip.py
@@ -3,6 +3,9 @@
 ### ZIP & UNZIP:
 def better_grouper(inputs, n):
     iters = [iter(inputs)] * n
+    for i in iters:
+        print(i)
+    print(iters)
     return zip(*iters)
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(list(better_grouper(nums, 2)))
